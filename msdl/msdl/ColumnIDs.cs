caf479f1289779f6987bd6ec7ecf239741dd8d7c
Michael <michael.kirmse91@gmail.com>
2013-06-08 18:16:00 +0200

    gridviewcolumns are now handled properly

---
+++ msdl/msdl/ColumnIDs.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace msdl
+{
+    internal class ColumnIDs
+    {
+        public int FileName { get; set; }
+        public int Progress { get; set; }
+        public int Status { get; set; }
+        public int ID { get; set; }
+        public int Button { get; set; }
+
+        public ColumnIDs(DataGridView grid)
+        {
+            FileName = grid.Columns["colFileName"].Index;
+            Progress = grid.Columns["colProgress"].Index;
+            Status = grid.Columns["colStatus"].Index;
+            ID = grid.Columns["colID"].Index;
+            Button = grid.Columns["colBtn"].Index;
+        }
+    }
+}
--- msdl/msdl/Form1.cs
+++ msdl/msdl/Form1.cs
@@ -10,11 +10,11 @@ using SharpBits.Base;
  *      High priority:
  *          build a progress bar column http://stackoverflow.com/questions/4646920/populating-a-datagridview-with-text-and-progressbars
  *          make the download directory configurable.
- *          remove the column IDs, exchange them for names.
  *          implement a timer to manage the labels on the buttons and stuff.
  *          implement a possibility to start a job with multiple files.
  *          implement a possibility to add files to a running job
  *          implement a possibility to change job priorities.
+ *          implement a possibility to remove jobs.
  *
  *      Low priority:
  *          build a tray status monitor.
@@ -25,6 +25,7 @@ namespace msdl
     public partial class Form1 : Form
     {
         private BitsManager _downloadManager;
+        private ColumnIDs _colIDs;
 
         public Form1()
         {
@@ -49,9 +50,15 @@ namespace msdl
                     AddJobFlags(job);
             }
 
+            _colIDs = new ColumnIDs(dataGridView1);
+
             dataGridView1.Rows.AddRange(GetDownloadJobsAsRows().ToArray());
         }
 
+        /// <summary>
+        /// Sets the necessary flags on a BitsJob to fire the events we want.
+        /// </summary>
+        /// <param name="job">The BitsJob whom to apply the flags.</param>
         private static void AddJobFlags(BitsJob job)
         {
             job.NotificationFlags = NotificationFlags.JobModified | NotificationFlags.JobTransferred |
@@ -63,10 +70,10 @@ namespace msdl
             e.Job.Complete();
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
-                if (e.Job.JobId.ToString() == row.Cells[4].Value.ToString())
-                    row.Cells[3].Value = e.Job.State.ToString();
+                if (e.Job.JobId.ToString() == row.Cells[_colIDs.ID].Value.ToString())
+                    row.Cells[_colIDs.Status].Value = e.Job.State.ToString();
             }
-            Console.WriteLine("Some job was transfered.");
+            Console.WriteLine(string.Format("Job {0} was transferred.", e.Job.JobId));
         }
 
 
@@ -74,14 +81,14 @@ namespace msdl
         {
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
-                if (e.Job.JobId.ToString() == row.Cells[4].Value.ToString())
+                if (e.Job.JobId.ToString() == row.Cells[_colIDs.ID].Value.ToString())
                 {
-                    row.Cells[2].Value = String.Format("{0}/{1}", e.Job.Progress.BytesTransferred, e.Job.Progress.BytesTotal);
-                    row.Cells[3].Value = e.Job.State.ToString();
+                    row.Cells[_colIDs.Progress].Value = String.Format("{0}/{1}", e.Job.Progress.BytesTransferred, e.Job.Progress.BytesTotal);
+                    row.Cells[_colIDs.Status].Value = e.Job.State.ToString();
                 }
             }
 
-            Console.WriteLine(string.Format("Job {0} was modified.", e.Job.JobId));
+            Console.WriteLine("Job {0} was modified.", e.Job.JobId);
         }
 
         private IEnumerable<DataGridViewRow> GetDownloadJobsAsRows()
@@ -93,13 +100,13 @@ namespace msdl
         {
             var row = new DataGridViewRow();
             row.CreateCells(dataGridView1);
-            row.Cells[0].Value = job.JobId;
-            row.Cells[1].Value = getButtonText(job);
-            if (row.Cells[1].Value.ToString() == "Done")
-                row.Cells[1].ReadOnly = true;
-            row.Cells[2].Value = String.Format("{0}/{1}", job.Progress.BytesTransferred, job.Progress.BytesTotal);
-            row.Cells[3].Value = job.State.ToString();
-            row.Cells[4].Value = job.JobId.ToString();
+            row.Cells[_colIDs.FileName].Value = job.JobId; // TODO: change this to get the filename rather then the download ID
+            row.Cells[_colIDs.Button].Value = getButtonText(job);
+            if (row.Cells[_colIDs.Button].Value.ToString() == "Done")
+                row.Cells[_colIDs.Button].ReadOnly = true;
+            row.Cells[_colIDs.Progress].Value = String.Format("{0}/{1}", job.Progress.BytesTransferred, job.Progress.BytesTotal);
+            row.Cells[_colIDs.Status].Value = job.State.ToString();
+            row.Cells[_colIDs.ID].Value = job.JobId.ToString();
             return row;
         }
 
@@ -145,10 +152,8 @@ namespace msdl
                 case 1: // aka button, do this properly!!!
                     foreach (var job in _downloadManager.Jobs.Values)
                     {
-                        if (dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString() == job.JobId.ToString())
-                        {
+                        if (dataGridView1.Rows[e.RowIndex].Cells[_colIDs.ID].Value.ToString() == job.JobId.ToString())
                             ManageJobState(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString(), job);
-                        }
                     }
                     break;
             }
